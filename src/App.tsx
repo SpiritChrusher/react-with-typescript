0c01d1fb5c51eda667e85ed94b1eea9bcfd11067
Szabó Kristóf <szabokristof997@gmail.com>
2021-07-16 17:27:04 +0200

    created the first state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React,  {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const[number, setNumber] = useState<number>(1);
+
+  const changeNumber = () => {
+    setNumber("1");
+  }
   return (
     <div className="App">
       <header className="App-header">
